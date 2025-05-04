84f80378004d46a2520986bbec442f2964bdfabe
Sang Tran <stran1025@gmail.com>
2025-05-04 15:47:23 -0700

    build fix

--- app/overview.tsx
+++ app/overview.tsx
@@ -2,7 +2,7 @@ function Overview() {
     return (
         <div className="relative overflow-x-auto">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                {/* Header */}
+                {/* --- HEADER --- */}
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
@@ -19,12 +19,12 @@ function Overview() {
                         </th>
                     </tr>
                 </thead>
-                {/* Header */}
+                {/* --- HEADER --- */}
                 <tbody>
                     {}
                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                            Apple MacBook Pro 17
                         </th>
                         <td className="px-6 py-4">
                             Silver
