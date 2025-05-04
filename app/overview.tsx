a0dd18bd73ea6f99d73e22f83a79e6a09cebe6a3
Sang Tran <stran1025@gmail.com>
2025-05-04 15:30:56 -0700

    init fix

--- app/overview.tsx
+++ app/overview.tsx
@@ -1,4 +1,4 @@
-function Overview(inventory) {
+function Overview() {
     return (
         <div className="relative overflow-x-auto">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
