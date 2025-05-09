c211e3bf0010411e57b702d5dd70bc10d3f096ac
Sang Tran <stran1025@gmail.com>
2025-05-08 18:56:46 -0700

    build fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
     "@tailwindcss/postcss": {},
-=======
-    '@tailwindcss/postcss': {},
->>>>>>> 4e3230a088b03788e724d2782e708cf737cee85d
     autoprefixer: {},
   },
 };
